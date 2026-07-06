5c095b70bb78a2e95a1fed59a6f8666c39190513
gandhi.ck <gandhi.ck@gmail.com>
2026-07-06 10:14:01 +0530

    Updating file to test workflow

--- src/Gateway/FeedR.Gateway/Program.cs
+++ src/Gateway/FeedR.Gateway/Program.cs
@@ -24,7 +24,7 @@ builder.Services
     
 var app = builder.Build();
 
-app.MapGet("/", () => "FeedR Gateway Workflow Testing 2");
+app.MapGet("/", () => "FeedR Gateway Workflow Testing 3");
 app.MapReverseProxy();
 
 app.Run();
